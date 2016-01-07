3dae68a5b287b365eb2aeb680318a94a58a89de4
Brendan J. Baker <brendan@brendanbaker.com>
2016-01-07 01:41:43 -0500

    TimeSpan.Microseconds() extension method.

---
+++ src/Bakery/Time/TimeSpanExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Bakery.Time
+{
+	using System;
+
+	public static class TimeSpanExtensions
+	{
+		public static Int64 Microseconds(this TimeSpan timeSpan)
+		{
+			const Int32 MICROSECONDS_PER_MILLISECOND = 1000;
+
+			var ticksPerMicrosecond = TimeSpan.TicksPerMillisecond / MICROSECONDS_PER_MILLISECOND;
+
+			return timeSpan.Ticks / ticksPerMicrosecond;
+		}
+	}
+}
