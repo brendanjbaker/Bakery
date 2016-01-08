9df6d254b427252508fad14d89fdc8948ba1f5c2
Brendan J. Baker <brendan@brendanbaker.com>
2016-01-07 21:57:19 -0500

    Uuid structure and String converter.

---
+++ src/Bakery/UuidTypeConverter.cs
@@ -0,0 +1,20 @@
+﻿namespace Bakery
+{
+	using System;
+	using System.ComponentModel;
+	using System.Globalization;
+
+	public class UuidTypeConverter
+		: TypeConverter
+	{
+		public override Boolean CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+		{
+			return sourceType == typeof(String);
+		}
+
+		public override Object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, Object value)
+		{
+			return new Uuid(Guid.Parse((String)value));
+		}
+	}
+}
