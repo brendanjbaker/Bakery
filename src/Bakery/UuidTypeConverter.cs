f830c8da35ecd30145d57e3232bee0d5ae6ec886
Brendan J. Baker <brendan@brendanbaker.com>
2016-01-07 22:11:34 -0500

    Implement TypeConverter CanConvertTo() and ConvertTo() methods for Uuid.

--- src/Bakery/UuidTypeConverter.cs
+++ src/Bakery/UuidTypeConverter.cs
@@ -12,9 +12,22 @@
 			return sourceType == typeof(String);
 		}
 
+		public override Boolean CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+		{
+			return destinationType == typeof(String);
+		}
+
 		public override Object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, Object value)
 		{
 			return new Uuid(Guid.Parse((String)value));
+		}
+
+		public override Object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, Object value, Type destinationType)
+		{
+			if (value == null)
+				return null;
+
+			return value.ToString();
 		}
 	}
 }
