420d6e58df9a6fcf9bceb6ccc730e9edb79b22ee
Brendan J. Baker <brendan@brendanbaker.com>
2016-01-09 00:57:00 -0500

    Logging abstraction with console-based implementation (aware of Stdin and Stderr).

---
+++ src/Bakery/Logging/ILog.cs
@@ -0,0 +1,9 @@
+﻿namespace Bakery.Logging
+{
+	using System;
+
+	public interface ILog
+	{
+		void Write(Level logLevel, String message);
+	}
+}
---
+++ src/Bakery/Logging/Level.cs
@@ -0,0 +1,10 @@
+﻿namespace Bakery.Logging
+{
+	public enum Level
+	{
+		Debug = 0x10,
+		Error = 0x80,
+		Information = 0x20,
+		Warning = 0x40
+	}
+}
