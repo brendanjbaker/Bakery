9f974197e5e306150f9b8904008d9e3b00183fad
Brendan J. Baker <brendan@brendanbaker.com>
2016-01-09 00:20:38 -0500

    Provide an SMTP configuration interface.

---
+++ src/Bakery/Mail/ISmtpConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace Bakery.Mail
+{
+	using System;
+
+	public interface ISmtpConfiguration
+	{
+		String Password { get; }
+		Int32 Port { get; }
+		String Server { get; }
+		String Username { get; }
+	}
+}
