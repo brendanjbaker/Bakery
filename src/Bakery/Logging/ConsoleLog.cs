979c4a3f6be79fe94bb7e0b0a0129e159f93df99
Brendan J. Baker <brendan@brendanbaker.com>
2016-01-09 01:04:17 -0500

    Enforce single responsibility principle -- decorators can implement additional functionality.

--- src/Bakery/Logging/ConsoleLog.cs
+++ src/Bakery/Logging/ConsoleLog.cs
@@ -1,32 +1,17 @@
 ﻿namespace Bakery.Logging
 {
 	using System;
-	using Time;
 
 	public class ConsoleLog
 		: ILog
 	{
-		private readonly IClock clock;
-
-		public ConsoleLog(IClock clock)
-		{
-			this.clock = clock;
-		}
-
 		public void Write(Level logLevel, String message)
 		{
-			const String FORMAT = "{0}: [{1}] {2}";
-
 			var textWriter = logLevel >= Level.Warning
 				? Console.Error
 				: Console.Out;
 
-			textWriter.WriteLine(
-				String.Format(
-					FORMAT,
-					clock.GetUniversalTime().ToString(),
-					logLevel,
-					message));
+			textWriter.WriteLine(message);
 		}
 	}
 }
