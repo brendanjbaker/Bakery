9e975bf2c6f5028dc4b613e27ebcad2ddd227177
Brendan J. Baker <brendan@brendanbaker.com>
2016-01-18 02:28:47 -0500

    Base-64 parsing and printing.

---
+++ src/Bakery/Text/IBase64Parser.cs
@@ -0,0 +1,8 @@
+﻿namespace Bakery.Text
+{
+	using System;
+
+	public interface IBase64Parser
+		: IParser<Byte[]>
+	{ }
+}
---
+++ src/Bakery/Text/SystemBase64Parser.cs
@@ -0,0 +1,20 @@
+﻿namespace Bakery.Text
+{
+	using System;
+
+	public class SystemBase64Parser
+		: IBase64Parser
+	{
+		public Byte[] TryParse(String @string)
+		{
+			try
+			{
+				return Convert.FromBase64String(@string);
+			}
+			catch
+			{
+				return null;
+			}
+		}
+	}
+}
