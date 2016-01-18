c22153696bd51431530a0c78ec58e027395ba825
Brendan J. Baker <brendan@brendanbaker.com>
2016-01-18 02:29:14 -0500

    Fixed-time "secure" byte array comparer.

---
+++ src/Bakery/Security/SecureByteArrayComparer.cs
@@ -0,0 +1,29 @@
+﻿namespace Bakery.Security
+{
+	using System;
+
+	public class SecureByteArrayComparer
+		: IByteArrayComparer
+	{
+		public Boolean IsEqual(Byte[] alpha, Byte[] beta)
+		{
+			if (alpha == null)
+				throw new ArgumentNullException(nameof(alpha));
+
+			if (beta == null)
+				throw new ArgumentNullException(nameof(beta));
+
+			if (alpha.Length != beta.Length)
+				return false;
+
+			var delta = 0;
+
+			for (var i = 0; i < alpha.Length; i++)
+			{
+				delta |= alpha[i] ^ beta[i];
+			}
+
+			return delta == 0;
+		}
+	}
+}
