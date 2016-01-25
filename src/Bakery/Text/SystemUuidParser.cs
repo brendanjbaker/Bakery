787990a602c33626bc08d8a8b5292edfcd2207f6
Brendan J. Baker <brendan@brendanbaker.com>
2016-01-24 19:48:36 -0500

    UUID parsing interface and system implementation.

---
+++ src/Bakery/Text/SystemUuidParser.cs
@@ -0,0 +1,18 @@
+﻿namespace Bakery.Text
+{
+	using System;
+
+	public class SystemUuidParser
+		: IUuidParser
+	{
+		public Uuid? TryParse(String @string)
+		{
+			Guid guid;
+
+			if (!Guid.TryParse(@string, out guid))
+				return null;
+
+			return new Uuid(guid);
+		}
+	}
+}
