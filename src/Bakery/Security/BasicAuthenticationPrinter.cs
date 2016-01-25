751df09a5d1ba9a7da3ce582030ec0dbeb5f7861
Brendan J. Baker <brendan@brendanbaker.com>
2016-01-25 03:05:33 -0500

    HTTP basic authentication header printer.

---
+++ src/Bakery/Security/BasicAuthenticationPrinter.cs
@@ -0,0 +1,24 @@
+﻿namespace Bakery.Security
+{
+	using System;
+	using System.Text;
+	using Text;
+
+	public class BasicAuthenticationPrinter
+		: IBasicAuthenticationPrinter
+	{
+		private readonly IBase64Printer base64Printer;
+		private readonly Encoding encoding;
+
+		public BasicAuthenticationPrinter(IBase64Printer base64Printer, Encoding encoding)
+		{
+			this.base64Printer = base64Printer;
+			this.encoding = encoding;
+		}
+
+		public String Print(IBasicAuthentication basicAuthentication)
+		{
+			return $"Basic {base64Printer.Print(encoding.GetBytes($"{basicAuthentication.Username}:{basicAuthentication.Password}"))}";
+		}
+	}
+}
