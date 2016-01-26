5f7abed0f6cfb2d62b698a8880c363ed933cc92c
Brendan J. Baker <brendan@brendanbaker.com>
2016-01-25 23:51:16 -0500

    JSON parsing abstractions.

---
+++ src/Bakery/Text/IJsonParser.cs
@@ -0,0 +1,10 @@
+﻿namespace Bakery.Text
+{
+	using System;
+
+	public interface IJsonParser
+		: IParser<Object>
+	{
+		T TryParse<T>(String @string);
+	}
+}
---
+++ src/Bakery/Text/JsonNetJsonParser.cs
@@ -0,0 +1,19 @@
+﻿namespace Bakery.Text
+{
+	using Newtonsoft.Json;
+	using System;
+
+	public class JsonNetJsonParser
+		: IJsonParser
+	{
+		public Object TryParse(String @string)
+		{
+			return TryParse<Object>(@string);
+		}
+
+		public T TryParse<T>(String @string)
+		{
+			return JsonConvert.DeserializeObject<T>(@string);
+		}
+	}
+}
