c3e8642fe212c9c50a096fa0e41e0a9a3fde1c9e
Brendan J. Baker <brendan@brendanbaker.com>
2016-02-19 02:00:54 -0500

    String.Remove(String) extension -- shorthand for replacing with String.Empty.

---
+++ src/Bakery/Text/StringRemoveExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Bakery.Text
+{
+	using System;
+
+	public static class StringRemoveExtensions
+	{
+		public static String Remove(this String @string, String @remove)
+		{
+			return @string.Replace(@remove, String.Empty);
+		}
+	}
+}
