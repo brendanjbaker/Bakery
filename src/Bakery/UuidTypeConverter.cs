55b62602552fb0c61fe31d4c51896ed8b8f562f5
Brendan J. Baker <brendan@brendanbaker.com>
2016-08-10 21:09:13 -0400

    Account for the possibility of exceptions when parsing UUIDs in UuidTypeConverter.

--- src/Bakery/UuidTypeConverter.cs
+++ src/Bakery/UuidTypeConverter.cs
@@ -14,7 +14,20 @@
 
 		public override Object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, Object value)
 		{
-			return new Uuid(Guid.Parse((String)value));
+			Guid guid;
+
+			if (value == null)
+				return null;
+
+			if (value.GetType() != typeof(String))
+				return null;
+
+			var valueText = value.ToString();
+
+			if (!Guid.TryParse(valueText, out guid))
+				return null;
+
+			return new Uuid(guid);
 		}
 	}
 }
