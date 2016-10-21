6e6a55634685f68e444aed932cf27d9b73cadc97
Brendan J. Baker <brendan@brendanbaker.com>
2016-10-21 00:39:27 -0400

    Provide a Parse() method on IJsonParser -- throws a ParseException (new) if parsing fails.

---
+++ src/Bakery/Text/ParseException.cs
@@ -0,0 +1,13 @@
+﻿namespace Bakery.Text
+{
+	using System;
+
+	public class ParseException<T>
+		: Exception
+	{
+		public override String Message
+		{
+			get { return $"Failed to parse type {typeof(T).Name}."; }
+		}
+	}
+}
