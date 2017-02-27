0941fa0b14b3da15499652282567ef26f3ea57d6
Brendan J. Baker <brendan@brendanbaker.com>
2017-02-26 23:15:09 -0500

    Extension for DateTime struct that allows retrieving its Unix timestamp value (in seconds or milliseconds).

---
+++ src/Bakery/Time/DateTimeExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Bakery.Time
+{
+	using System;
+
+	public static class DateTimeExtensions
+	{
+		private static readonly DateTime UNIX_EPOCH = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+		public static Int64 ToUnixTimestamp(this DateTime instance, Boolean useMilliseconds = false)
+		{
+			var elapsed = instance.ToUniversalTime() - UNIX_EPOCH;
+
+			return useMilliseconds
+				? (Int64)elapsed.TotalMilliseconds
+				: (Int64)elapsed.TotalSeconds;
+		}
+	}
+}
