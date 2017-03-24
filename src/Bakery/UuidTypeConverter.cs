e15d21475146134fb48510c0402390d2bc3e607c
Brendan J. Baker <brendan@brendanbaker.com>
2017-03-23 20:02:55 -0400

    Implement ConvertTo(), etc., in UuidTypeConverter; refactoring as appropriate.

--- src/Bakery/UuidTypeConverter.cs
+++ src/Bakery/UuidTypeConverter.cs
@@ -9,22 +9,46 @@
 	{
 		public override Boolean CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
 		{
-			return sourceType == typeof(String);
+			return
+				sourceType == typeof(String) ||
+				sourceType == typeof(Guid);
+		}
+
+		public override Boolean CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+		{
+			return destinationType == typeof(Uuid);
 		}
 
 		public override Object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, Object value)
 		{
-			Guid guid;
-
 			if (value == null)
 				return null;
 
-			if (value.GetType() != typeof(String))
+			if (value.GetType() == typeof(String))
+				return FromString(value.ToString());
+
+			if (value.GetType() == typeof(Guid))
+				return new Uuid((Guid)value);
+
+			return null;
+		}
+
+		public override Object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, Object value, Type destinationType)
+		{
+			if (value == null)
 				return null;
 
-			var valueText = value.ToString();
+			if (value.GetType() == typeof(Guid))
+				return new Uuid((Guid)value);
 
-			if (!Guid.TryParse(valueText, out guid))
+			return null;
+		}
+
+		private static Object FromString(String @string)
+		{
+			Guid guid;
+
+			if (!Guid.TryParse(@string, out guid))
 				return null;
 
 			return new Uuid(guid);
