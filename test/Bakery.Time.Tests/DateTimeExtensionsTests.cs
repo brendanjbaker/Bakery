02948377398c552a4789b8047c7f3d96365b1b76
Brendan J. Baker <brendan@brendanbaker.com>
2017-03-31 02:10:57 -0400

    DateTimeExtensions extension method tests.

---
+++ test/Bakery.Time.Tests/DateTimeExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xunit;
+
+public class DateTimeExtensionsTests
+{
+	[Fact]
+	public void UnixEpochIsZero()
+	{
+		var unixEpoch = new DateTime(1970, 1, 1, 00, 00, 00, DateTimeKind.Utc);
+
+		Assert.True(unixEpoch.ToUnixTimestamp() == 0);
+	}
+
+	[Fact]
+	public void OneSecondAfterUnixEpochIsOne()
+	{
+		var unixEpoch = new DateTime(1970, 1, 1, 00, 00, 01, DateTimeKind.Utc);
+
+		Assert.True(unixEpoch.ToUnixTimestamp() == 1);
+	}
+
+	[Fact]
+	public void OneSecondAfterUnixEpochInMillisecondsIsOneThousand()
+	{
+		var unixEpoch = new DateTime(1970, 1, 1, 00, 00, 01, DateTimeKind.Utc);
+
+		Assert.True(unixEpoch.ToUnixTimestamp(true) == 1000);
+	}
+
+	[Fact]
+	public void OneSecondBeforeUnixEpochIsNegativeOne()
+	{
+		var unixEpoch = new DateTime(1969, 12, 31, 23, 59, 59, DateTimeKind.Utc);
+
+		Assert.True(unixEpoch.ToUnixTimestamp() == -1);
+	}
+}
