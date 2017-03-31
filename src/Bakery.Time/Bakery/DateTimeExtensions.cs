83665003dd766be94f837258cb6a34c5d16027fc
Brendan J. Baker <brendan@brendanbaker.com>
2017-03-31 01:38:48 -0400

    Remove namespace from DateTime and TimeSpan extension methods. This makes them globally available (without a "using" directive).

--- src/Bakery.Time/Bakery/DateTimeExtensions.cs
+++ src/Bakery.Time/Bakery/DateTimeExtensions.cs
@@ -1,18 +1,15 @@
-﻿namespace Bakery
+﻿using System;
+
+public static class DateTimeExtensions
 {
-	using System;
+	private static readonly DateTime UNIX_EPOCH = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
-	public static class DateTimeExtensions
+	public static Int64 ToUnixTimestamp(this DateTime instance, Boolean useMilliseconds = false)
 	{
-		private static readonly DateTime UNIX_EPOCH = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+		var elapsed = instance.ToUniversalTime() - UNIX_EPOCH;
 
-		public static Int64 ToUnixTimestamp(this DateTime instance, Boolean useMilliseconds = false)
-		{
-			var elapsed = instance.ToUniversalTime() - UNIX_EPOCH;
-
-			return useMilliseconds
-				? (Int64)elapsed.TotalMilliseconds
-				: (Int64)elapsed.TotalSeconds;
-		}
+		return useMilliseconds
+			? (Int64)elapsed.TotalMilliseconds
+			: (Int64)elapsed.TotalSeconds;
 	}
 }
--- src/Bakery.Time/Bakery/TimeSpanExtensions.cs
+++ src/Bakery.Time/Bakery/TimeSpanExtensions.cs
@@ -1,16 +1,13 @@
-﻿namespace Bakery
+﻿using System;
+
+public static class TimeSpanExtensions
 {
-	using System;
-
-	public static class TimeSpanExtensions
+	public static Int64 Microseconds(this TimeSpan timeSpan)
 	{
-		public static Int64 Microseconds(this TimeSpan timeSpan)
-		{
-			const Int32 MICROSECONDS_PER_MILLISECOND = 1000;
+		const Int32 MICROSECONDS_PER_MILLISECOND = 1000;
 
-			var ticksPerMicrosecond = TimeSpan.TicksPerMillisecond / MICROSECONDS_PER_MILLISECOND;
+		var ticksPerMicrosecond = TimeSpan.TicksPerMillisecond / MICROSECONDS_PER_MILLISECOND;
 
-			return timeSpan.Ticks / ticksPerMicrosecond;
-		}
+		return timeSpan.Ticks / ticksPerMicrosecond;
 	}
 }
