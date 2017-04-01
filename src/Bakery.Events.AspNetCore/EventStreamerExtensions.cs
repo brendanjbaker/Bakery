08aadb5ffed5cda61538f27d017b7cd9b3b5ee62
Brendan J. Baker <brendan@brendanbaker.com>
2017-04-01 18:10:55 -0400

    Add Bakery.Events.AspNetCore project: Provides convenience mechanisms to stream events, etc., in ASP.NET Core via WebSockets.

---
+++ src/Bakery.Events.AspNetCore/EventStreamerExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Bakery.Events;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+public static class EventStreamerExtensions
+{
+	public static async Task StreamAsync(this IEventStreamer eventStreamer, String topic, HttpContext httpContext)
+	{
+		var buffer = new ArraySegment<Byte>(new Byte[4096]);
+		var webSocket = await httpContext.WebSockets.AcceptWebSocketAsync();
+
+		await eventStreamer.StreamAsync(topic, httpContext.RequestAborted, async message =>
+		{
+			await webSocket.SendTextAsync(message);
+
+			var receiveResult = await webSocket.ReceiveAsync(buffer, httpContext.RequestAborted);
+
+			if (receiveResult.CloseStatus.HasValue)
+				httpContext.Abort();
+		});
+	}
+}
