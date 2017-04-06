d5189b2dd995f45ba265f8b469542e1d6575a8de
Brendan J. Baker <brendan@brendanbaker.com>
2017-04-05 22:58:43 -0400

    Bakery.Processes -- abstractions for running external processes, and sending/receiving text to/from them, etc.

---
+++ src/Bakery.Processes/Bakery/Processes/Output.cs
@@ -0,0 +1,20 @@
+﻿namespace Bakery.Processes
+{
+	using System;
+
+	public struct Output
+	{
+		private readonly String text;
+		private readonly OutputType type;
+
+		public Output(OutputType type, String text)
+		{
+			this.type = type;
+			this.text = text;
+		}
+
+		public String Text => text;
+
+		public OutputType Type => type;
+	}
+}
---
+++ src/Bakery.Processes/ProcessFactoryExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Bakery.Processes;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+public static class ProcessFactoryExtensions
+{
+	public static async Task<IProcess> RunAsync(this IProcessFactory processFactory, ProcessStartInfo processStartInfo)
+	{
+		return await processFactory.RunAsync(processStartInfo, TimeSpan.FromMinutes(1));
+	}
+
+	public static async Task<IProcess> RunAsync(this IProcessFactory processFactory, ProcessStartInfo processStartInfo, TimeSpan timeout)
+	{
+		var process = processFactory.Start(processStartInfo);
+
+		await process.WaitForExit(timeout);
+
+		return process;
+	}
+}
