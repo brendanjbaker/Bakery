5e38c5dd85225ca64e80f21b2e54ec53ff13b626
Brendan J. Baker <brendan@brendanbaker.com>
2017-04-05 22:56:25 -0400

    Bakery.Exceptions project, which currently includes ArgumentNegativeException and ArgumentGreaterThanOtherArgumentException.

---
+++ src/Bakery.Exceptions/Bakery/Exceptions/ArgumentGreaterThanOtherArgumentException.cs
@@ -0,0 +1,11 @@
+﻿namespace Bakery.Exceptions
+{
+	using System;
+
+	public class ArgumentGreaterThanOtherArgumentException
+		: ArgumentOutOfRangeException
+	{
+		public ArgumentGreaterThanOtherArgumentException(String argumentName, String otherArgumentName)
+			: base(argumentName, $@"May not be greater than argument ""{otherArgumentName}"".") { }
+	}
+}
---
+++ src/Bakery.Exceptions/Bakery/Exceptions/ArgumentNegativeException.cs
@@ -0,0 +1,11 @@
+﻿namespace Bakery.Exceptions
+{
+	using System;
+
+	public class ArgumentNegativeException
+		: ArgumentOutOfRangeException
+	{
+		public ArgumentNegativeException(String argumentName)
+			: base(argumentName, "May not be negative.") { }
+	}
+}
