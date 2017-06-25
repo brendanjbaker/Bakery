c1944eda18c78e31a6f542fadcdd8bc61194c50d
Brendan J. Baker <brendan@brendanbaker.com>
2017-06-25 17:45:55 -0400

    Process specification convenience extension methods.

--- src/Bakery.Processes/ProcessFactoryExtensions.cs
+++ src/Bakery.Processes/ProcessFactoryExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Bakery.Processes;
 using Bakery.Processes.Specification;
+using Bakery.Processes.Specification.Builder;
 using System;
 using System.Threading.Tasks;
 
@@ -10,6 +11,13 @@ public static class ProcessFactoryExtensions
 		return await processFactory.RunAsync(processSpecification, TimeSpan.FromMinutes(1));
 	}
 
+	public static async Task<IProcess> RunAsync(this IProcessFactory processFactory, Func<IProcessSpecificationBuilder, IProcessSpecification> processSpecificationBuilder)
+	{
+		var processSpecification = processSpecificationBuilder(ProcessSpecificationBuilder.Create());
+
+		return await processFactory.RunAsync(processSpecification);
+	}
+
 	public static async Task<IProcess> RunAsync(this IProcessFactory processFactory, IProcessSpecification processSpecification, TimeSpan timeout)
 	{
 		var process = processFactory.Start(processSpecification);
@@ -17,5 +25,12 @@ public static class ProcessFactoryExtensions
 		await process.WaitForExit(timeout);
 
 		return process;
+	}
+
+	public static IStartedProcess Start(this IProcessFactory processFactory, Func<IProcessSpecificationBuilder, IProcessSpecification> processSpecificationBuilder)
+	{
+		var processSpecification = processSpecificationBuilder(ProcessSpecificationBuilder.Create());
+
+		return processFactory.Start(processSpecification);
 	}
 }
