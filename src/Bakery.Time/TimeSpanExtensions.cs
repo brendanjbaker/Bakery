122df808cfc7adda83ef39fc577b3ebee43d6c01
Brendan J. Baker <brendan@brendanbaker.com>
2017-06-25 13:06:57 -0400

    Use constants, where possible.

--- src/Bakery.Time/TimeSpanExtensions.cs
+++ src/Bakery.Time/TimeSpanExtensions.cs
@@ -5,9 +5,8 @@ public static class TimeSpanExtensions
 	public static Int64 Microseconds(this TimeSpan timeSpan)
 	{
 		const Int32 MICROSECONDS_PER_MILLISECOND = 1000;
+		const Int64 TICKS_PER_MICROSECOND = TimeSpan.TicksPerMillisecond / MICROSECONDS_PER_MILLISECOND;
 
-		var ticksPerMicrosecond = TimeSpan.TicksPerMillisecond / MICROSECONDS_PER_MILLISECOND;
-
-		return timeSpan.Ticks / ticksPerMicrosecond;
+		return timeSpan.Ticks / TICKS_PER_MICROSECOND;
 	}
 }
