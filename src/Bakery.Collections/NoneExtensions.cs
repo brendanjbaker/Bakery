31b73e8c7b48b8661096b3b9d15747dae5c80d56
Brendan J. Baker <brendan@brendanbaker.com>
2017-07-24 21:23:17 -0400

    Extension method for collections: None(). Opposite of Any().

---
+++ src/Bakery.Collections/NoneExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class NoneExtensions
+{
+	public static Boolean None<T>(this IEnumerable<T> items)
+	{
+		return !items.Any();
+	}
+}
