eda78cbfb4a5d0cc00c74787fd6629f1edaac2b6
Brendan J. Baker <brendan@brendanbaker.com>
2017-07-25 00:32:19 -0400

    AbsoluteCache and InfiniteCache implementations of ICache.

---
+++ src/Bakery.Caching/Bakery/Caching/InfiniteCache.cs
@@ -0,0 +1,18 @@
+﻿namespace Bakery.Caching
+{
+	public class InfiniteCache<T>
+		: ICache<T>
+	{
+		private T item;
+
+		public T TryRead()
+		{
+			return item;
+		}
+
+		public void Write(T item)
+		{
+			this.item = item;
+		}
+	}
+}
