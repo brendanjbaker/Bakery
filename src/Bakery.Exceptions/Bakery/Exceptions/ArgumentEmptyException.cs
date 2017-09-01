e94d648fb2df3d68bb77c6b129eae870d7d2a17c
Brendan J. Baker <brendan@brendanbaker.com>
2017-09-01 10:16:30 -0400

    Add an exception that represents an argument that may not be empty.

---
+++ src/Bakery.Exceptions/Bakery/Exceptions/ArgumentEmptyException.cs
@@ -0,0 +1,11 @@
+﻿namespace Bakery.Exceptions
+{
+	using System;
+
+	public class ArgumentEmptyException
+		: ArgumentException
+	{
+		public ArgumentEmptyException(String argumentName)
+			: base("May not be empty.", argumentName) { }
+	}
+}
