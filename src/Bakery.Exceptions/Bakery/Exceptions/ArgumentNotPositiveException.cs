839d6abaa73db0031a27118c6edfe3bcdf9c274d
Brendan J. Baker <brendan@brendanbaker.com>
2017-09-01 10:16:48 -0400

    Add an exception that represents an argument that must be positive.

---
+++ src/Bakery.Exceptions/Bakery/Exceptions/ArgumentNotPositiveException.cs
@@ -0,0 +1,11 @@
+﻿namespace Bakery.Exceptions
+{
+	using System;
+
+	public class ArgumentNotPositiveException
+		: ArgumentOutOfRangeException
+	{
+		public ArgumentNotPositiveException(String argumentName)
+			: base(argumentName, "Must be positive.") { }
+	}
+}
