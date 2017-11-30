825a9ad06c5f3bc6afd98a665f50b04e2b26e408
Brendan J. Baker <brendan@brendanbaker.com>
2017-11-30 11:55:44 -0500

    Bakery.Transput project -- includes a IReader<T> and IWriter<T> interfaces.

---
+++ Bakery.Transput/Bakery/Transput/ConsoleWriter.cs
@@ -0,0 +1,15 @@
+﻿namespace Bakery.Transput
+{
+	using System;
+	using System.Threading;
+	using System.Threading.Tasks;
+
+	public class ConsoleWriter<T>
+		: IWriter<T>
+	{
+		public async Task WriteAsync(T item, CancellationToken cancellationToken)
+		{
+			await Console.Out.WriteLineAsync(item.ToString());
+		}
+	}
+}
---
+++ Bakery.Transput/WriterExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Bakery.Transput;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+public static class WriterExtensions
+{
+	public static void Write<T>(this IWriter<T> writer, T item)
+	{
+		writer.WriteAsync(item, CancellationToken.None).GetAwaiter().GetResult();
+		writer.Write(item, CancellationToken.None);
+	}
+
+	public static void Write<T>(this IWriter<T> writer, T item, CancellationToken cancellationToken)
+	{
+		writer.WriteAsync(item, cancellationToken).GetAwaiter().GetResult();
+	}
+
+	public static async Task WriteAsync<T>(this IWriter<T> writer, T item)
+	{
+		await writer.WriteAsync(item, CancellationToken.None);
+	}
+
+	public static async Task WriteAsync<T>(this IWriter<T> writer, T item, TimeSpan timeout)
+	{
+		using (var cancellationTokenSource = new CancellationTokenSource(timeout))
+			await writer.WriteAsync(item, cancellationTokenSource.Token);
+	}
+}
