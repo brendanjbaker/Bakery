ef3b1f898393d35acff19b414da2f409cfbfd72d
Brendan J. Baker <brendan@brendanbaker.com>
2017-12-03 21:20:30 -0500

    Add Bakery.Programs project -- supports standardized cancellation, dependency injection, etc.

---
+++ src/Bakery.Programs/Bakery/Programs/IProgram.cs
@@ -0,0 +1,10 @@
+﻿namespace Bakery.Programs
+{
+	using System.Threading;
+	using System.Threading.Tasks;
+
+	public interface IProgram
+	{
+		Task RunAsync(CancellationToken cancellationToken);
+	}
+}
---
+++ src/Bakery.Programs/Bakery/Programs/IProgramBuilder.cs
@@ -0,0 +1,17 @@
+﻿namespace Bakery.Programs
+{
+	using Bakery.Composition.SimpleInjector;
+	using System;
+	using System.Threading.Tasks;
+
+	public interface IProgramBuilder
+	{
+		IProgramBuilder ExitOnControlC(Boolean enabled = true);
+		IProgramBuilder DelayAfterUnhandledException(TimeSpan delay);
+		IProgramBuilder RegisterModule<TModule>() where TModule : IModule, new();
+		IProgramBuilder UseProgram<TProgram>() where TProgram : IProgram;
+
+		void Run();
+		Task RunAsync();
+	}
+}
