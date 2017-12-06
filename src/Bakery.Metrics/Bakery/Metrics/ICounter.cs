2ae5ffb8e0e42af171b8369ffa64d5aca26ea17a
Brendan J. Baker <brendan@brendanbaker.com>
2017-12-06 00:28:55 -0500

    Bakery.Metrics project -- supports incrementing and reading counters. Currently provides a naive in-memory implementation.

---
+++ src/Bakery.Metrics/Bakery/Metrics/ICounter.cs
@@ -0,0 +1,11 @@
+﻿namespace Bakery.Metrics
+{
+	using System;
+	using System.Threading.Tasks;
+
+	public interface ICounter
+	{
+		Task IncrementAsync(Object key, Int64 count = 1);
+		Task<Int64> ReadAsync(Object key);
+	}
+}
---
+++ src/Bakery.Metrics/Bakery/Metrics/MemoryCounter.cs
@@ -0,0 +1,44 @@
+﻿namespace Bakery.Metrics
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Threading.Tasks;
+
+	public class MemoryCounter
+		: ICounter
+	{
+		private readonly IDictionary<Object, Int64> counts;
+
+		public MemoryCounter()
+			: this(new Dictionary<Object, Int64>()) { }
+
+		private MemoryCounter(IDictionary<Object, Int64> counts)
+		{
+			this.counts = counts ?? throw new ArgumentNullException(nameof(counts));
+		}
+
+		public Task IncrementAsync(Object key, Int64 count = 1)
+		{
+			lock (counts)
+			{
+				if (!counts.ContainsKey(key))
+					counts.Add(key, 0);
+
+				counts[key] += count;
+			}
+
+			return Task.CompletedTask;
+		}
+
+		public Task<Int64> ReadAsync(Object key)
+		{
+			Int64 count;
+
+			lock (counts)
+				if (!counts.TryGetValue(key, out count))
+					count = 0;
+
+			return Task.FromResult(count);
+		}
+	}
+}
