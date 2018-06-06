47cfeb4fb8c20d0f178c20b6304c7d2224563b4c
Brendan J. Baker <brendan@brendanbaker.com>
2018-06-06 01:27:33 -0400

    Object.Is<T>() and Object.Cast<T>() extension methods.

---
+++ src/Bakery.Reflection/ObjectExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public static class ObjectExtensions
+{
+	public static T Cast<T>(this Object @object)
+	{
+		// Casting a null instance is usually permissible, but since we're using extension method
+		// syntax, we'll disallow it here.
+
+		if (@object == null)
+			throw new ArgumentNullException(nameof(@object));
+
+		return (T)@object;
+	}
+
+	public static Boolean Is<T>(this Object @object)
+	{
+		if (@object == null)
+			throw new ArgumentNullException(nameof(@object));
+
+		return @object.GetType().IsAssignableFrom(typeof(T));
+	}
+}
