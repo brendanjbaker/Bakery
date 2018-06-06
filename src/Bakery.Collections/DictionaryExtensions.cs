e315d3a9f6598a5a6ade113382ee71a46db68868
Brendan J. Baker <brendan@brendanbaker.com>
2018-06-06 01:06:47 -0400

    Dictionary.Clone() extension method.

---
+++ src/Bakery.Collections/DictionaryExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+public static class DictionaryExtensions
+{
+	public static IDictionary<TKey, TValue> Clone<TKey, TValue>(this IDictionary<TKey, TValue> dictionary)
+	{
+		return new Dictionary<TKey, TValue>(dictionary);
+	}
+}
